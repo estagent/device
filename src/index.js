24651dd3b664cf2f47b0c64a5584e1448ba0ac79
estagent <estagent@chip.me>
2021-01-04 13:47:48 +0300

    trackers, identifications moved out to revgaming/trackers

--- src/index.js
+++ src/index.js
@@ -1,72 +1,19 @@
-import {getEventDetail} from '@revgaming/helpers'
-import {Events} from '@revgaming/session'
-
 import Bowser from 'bowser'
-import {
-  setCurrentAgent,
-  getAgentIdentification,
-  updateAgentIdentification,
-  incrementAgentCounter,
-  getAgents,
-} from './agents'
-import {
-  setCurrentUser,
-  getUserIdentification,
-  updateUserIdentification,
-  getUsers,
-  incrementUserCounter,
-} from './users'
-import {getIdentifier, getIdentifications} from './identifications'
 
 let parser
-
-export {getIdentifier, getIdentifications}
 
 export const bootDevice = (opts = {}) => {
   const UA = window.navigator.userAgent
   parser = Bowser.getParser(UA)
-  setCurrentAgent(UA)
-  if (opts.hasOwnProperty('listeners') && opts.listeners instanceof Array) {
-    if (opts.listeners.includes('agents')) trackAgents()
-    if (opts.listeners.includes('users')) trackUsers()
-  }
   return {
-    getAgentParser: () => parser,
-    getAgentResult: () => parser.getResult(),
-    getUserAgent: () => parser.getUA(),
-    getBrowser: () => parser.getBrowser(),
-    getPlatform: () => parser.getPlatform(),
-    getOS: () => parser.getOS(),
-    isMobile: isMobile,
-    isDesktop: isDesktop,
-    isTablet: isTablet,
-    isAgent: is,
-    getDeviceId: getIdentifier,
-    getIdentifications: getIdentifications,
-    getAgentIdentification: getAgentIdentification,
-    updateAgentIdentification: updateAgentIdentification,
-    incrementAgentCounter: incrementAgentCounter,
-    getUserIdentification: getUserIdentification,
-    updateUserIdentification: updateUserIdentification,
-    incrementUserCounter: incrementUserCounter,
-    getAgents: getAgents,
-    getUsers: getUsers,
+    agentParser: agentParser,
+    agentResult: agentResult,
+    userAgent: userAgent,
+    agentIs: agentIs,
   }
 }
-export const isMobile = () => parser.isPlatform('mobile')
-export const isDesktop = () => parser.isPlatform('desktop')
-export const isTablet = () => parser.isPlatform('tablet')
-export const is = (string, withAliases = false) =>
+export const agentParser = () => parser
+export const agentResult = () => parser.getResult()
+export const userAgent = () => parser.getUA()
+export const agentIs = (string, withAliases = false) =>
   parser.is(string, withAliases)
-
-const trackAgents = () =>
-  window.addEventListener(Events.SessionCreated, () =>
-    incrementAgentCounter('sessions'),
-  )
-
-const trackUsers = () => {
-  window.addEventListener(Events.UserAuthenticated, function (event) {
-    const user = getEventDetail(event, 'user')
-    if (user) setCurrentUser(user.uuid ?? user.id)
-  })
-}
